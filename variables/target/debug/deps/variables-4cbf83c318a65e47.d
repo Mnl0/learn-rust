58a570d414c30c7b9c375ec08c33fb0f3814d4e2
Manuel <68096344+Mnl0@users.noreply.github.com>
2024-05-01 12:42:19 -0400

    Delete variables/target directory

--- variables/target/debug/deps/variables-4cbf83c318a65e47.d
+++
@@ -1,5 +0,0 @@
-D:\Manuel\Escritorio\rust-project\variables\target\debug\deps\libvariables-4cbf83c318a65e47.rmeta: src\main.rs
-
-D:\Manuel\Escritorio\rust-project\variables\target\debug\deps\variables-4cbf83c318a65e47.d: src\main.rs
-
-src\main.rs:
--- variables/target/debug/deps/variables.d
+++
@@ -1,5 +0,0 @@
-D:\Manuel\Escritorio\rust-project\variables\target\debug\deps\variables.exe: src\main.rs
-
-D:\Manuel\Escritorio\rust-project\variables\target\debug\deps\variables.d: src\main.rs
-
-src\main.rs:
